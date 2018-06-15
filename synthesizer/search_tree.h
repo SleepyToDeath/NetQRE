52f2d47c58bad97b70f66532001c4fb85645625d
Lei Shi <sleepytodeath@gmail.com>
2018-06-15 17:17:22 -0400

    define syntax tree and search tree

--- synthesizer/search_tree.h
+++ synthesizer/search_tree.h
@@ -1,20 +1,87 @@
 #ifndef SEARCH_TREE_H
 #define SEARCH_TREE_H
 
+#include "syntax_tree.h"
 
-class LNode {
-	public:
-
+enum SearchNodeColor {
+	White, Black, Gray
 };
 
-class DNode {
-	public:
+class RNode;
+class DNode;
+class SearchState;
 
+class SearchTreeNode
+{
+	public:
+	SearchState state;
+
+	SearchTreeNode();
+	virtual bool search() = 0;
+	bool is_feasible();
+	SearchNodeColor get_color();
+
+	protected:
+	SearchNodeColor color;
+	bool feasible;
 };
 
-class RNode {
+class LNode: public SearchTreeNode {
 	public:
+	SyntaxLeftHandSide* syntax;
+	std::vector<DNode*> option;
 
+	bool search();
+	bool accept(SyntaxTree* t);
 };
+
+class DNode: public SearchTreeNode {
+	public:
+	SyntaxRightHandSide* syntax;
+	DivideStrategy* divider;
+	std::vector<RNode*> option;
+
+	bool search();
+};
+
+class RNode: public SearchTreeNode {
+	public:
+	SyntaxRightHandSide* syntax;
+	std::vector<LNode*> subexp;
+
+	bool search();
+};
+
+class SearchContext {
+	std::map<SearchState,LNode*> cache;
+};
+
+/* ========================= Domain Specific Contents ==================== */
+
+/* one for each language */
+class SearchState {
+};
+
+/* one for each right hand side option */
+class DivideStrategy {
+	public:
+	/* for independent rule */
+	/* substates[i,j]: in ith dividing option, jth subexp get this state */
+	virtual std::vector< std::vector<SearchState> > get_indep_substates(SearchState s) = 0;
+
+	/* for dependent rule */
+	/* We model this kind of search as finding proper smaller non-overlapping intervals
+		that togeter FULLY covers a whole larger interval.
+		get_min returns the lower bound of the larger interval.
+		get_max returns the upper bound of the larger interval.
+		get_dep_substates returns all possible substates that can cover a small interval.
+		All intervals here are left-closed right-open. */
+		
+	virtual int get_min(SearchState s) = 0;
+	virtual int get_max(SearchState s) = 0;
+	virtual std::vector<SearchState> get_dep_substates(SearchState s, int min, int max) = 0;
+};
+
+/* ======================================================================= */
 
 #endif
--- synthesizer/syntax_tree.h
+++ synthesizer/syntax_tree.h
@@ -3,28 +3,60 @@
 
 #include<vector>
 
-class SyntaxNode;
+class SyntaxTreeNode;
 class SyntaxRule;
 
 class SyntaxTree {
 	public:
-	std::vector<
+	SyntaxTree();
+	SyntaxTree(SyntaxTree* t); /* copy constructor */
 
+	void mutate(int option);
+	
+	SyntaxTreeNode* root;
+	std::vector<SyntaxTree*> subtree;
 };
 
-class SyntaxNode {
+class SyntaxTreeNode {
 
 	public:
-	SyntaxRule type;
+	SyntaxTreeNode(SyntaxRule* type);
+	SyntaxRule* get_type();
+	void set_option();
+	int get_option();
+	
+	private:
+	SyntaxRule* type;
+	int option;
+};
+
+class LanguageSyntax
+{
+	public:
+	int add_rule(SyntaxLeftHandSide* r);
+	SyntaxLeftHandSide* get_rule(int id);
+	int size();
 
 	private:
-
+	vector<SyntaxLeftHandSide*> rule;
 };
 
-class SyntaxRule {
+class SyntaxLeftHandSide {
 	public:
-	int num_sub_exp;
-	bool terminal;
+	int id;
+	int size();
+	vector<SyntaxRightHandSide*> option;
+	bool is_term;
+
+	const int NoOption = -1;
 };
+
+class SyntaxRightHandSide {
+	public:
+	int size();
+	bool independent; /* only support one dependent subexp, which must be the only subexp */
+	vector<SyntaxLeftHandSide*> subexp;
+};
+
 
 #endif
