724309b1455ed3010050f31594fefa5e59035fa6
Lei Shi <sleepytodeath@gmail.com>
2020-07-05 20:29:31 -0400

    tmp

--- netqre2dt/rpc/distribute.sh
+++ netqre2dt/rpc/distribute.sh
@@ -3,12 +3,12 @@ cp $1 ./positive_train.ts
 cp $2 ./negative_train.ts
 cp $3 ./positive_test.ts
 cp $4 ./negative_test.ts
-for ((i=2; i<=$5; i++))
-do
-	scp ./Makefile n$i:rpc/
-	scp ./netqre_server n$i:rpc/
-	scp ./positive_train.ts n$i:rpc/
-	scp ./negative_train.ts n$i:rpc/
-	scp ./positive_test.ts n$i:rpc/
-	scp ./negative_test.ts n$i:rpc/
-done
+#for ((i=2; i<=$5; i++))
+#do
+#	scp ./Makefile n$i:rpc/
+#	scp ./netqre_server n$i:rpc/
+#	scp ./positive_train.ts n$i:rpc/
+#	scp ./negative_train.ts n$i:rpc/
+#	scp ./positive_test.ts n$i:rpc/
+#	scp ./negative_test.ts n$i:rpc/
+#done
--- netqre2dt/rpc/killall.sh
+++ netqre2dt/rpc/killall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-make kill
-for ((i=2; i<=$1; i++))
-do
-	ssh n$i "cd rpc; make kill" & 
-done
+#make kill
+#for ((i=2; i<=$1; i++))
+#do
+#	ssh n$i "cd rpc; make kill" & 
+#done
